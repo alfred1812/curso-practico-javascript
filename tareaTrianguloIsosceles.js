7ddc72462dff271f7eb7e2bef4692ea444c7dee8
Alfredo Paucara <tteciv@gmail.com>
2022-07-14 15:09:57 -0400

    se agrego tarea sobre hallar la altura de trianbulo ISOSCELES

---
+++ tareaTrianguloIsosceles.js
@@ -0,0 +1,26 @@
+function alturaIsosceles(ladoA, ladoB, base){
+    if (ladoA!=ladoB){
+        
+        return console.error("no es un triangulo Isosceles");
+    }
+    else{
+        return Math.sqrt(Math.pow(ladoA, 2) - Math.pow(base, 2));
+    }
+}
+
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
